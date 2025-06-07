4abcd4da01f41d11de20c848342d9a5faba13368
agus-darmawan <wayanagus.dr@gmail.com>
2025-06-08 02:04:11 +0700

    feat: add not found page

---
+++ src/constant/config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: "Patrol Monitoring System",
+  description:
+    "A Patrol Monitoring System for Thermal monitoring in electrical substations",
+  url: "https://patrol.agus-darmawan.com",
+};
