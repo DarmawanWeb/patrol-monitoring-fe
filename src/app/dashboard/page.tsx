e0f18176477d42f1fb68f830243546fe86396f35
agus-darmawan <wayanagus.dr@gmail.com>
2025-06-29 22:19:48 +0700

    fix: lint and biome error formating

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,9 @@
 "use client";
-import { useAuthContext } from "@/components/provider/auth-provider";
 import BottomMenu from "@/components/shared/bottom-menu";
 import GridMap from "@/components/shared/grid-maps";
 import Navbar from "@/components/shared/navbar";
 
 export default function Dashboard() {
-  const { user, logout } = useAuthContext();
-
   return (
     <main>
       <Navbar />
